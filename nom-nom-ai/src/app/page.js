e16258bfb77a0f9d5ba320d1131b98e34df030e3
ellaly2 <ellaly2220@gmail.com>
2025-02-14 12:07:21 -0800

    button added

--- nom-nom-ai/src/app/page.js
+++ nom-nom-ai/src/app/page.js
@@ -1,26 +1,23 @@
 import Image from "next/image";
 import "./App.css";
 
-
 export default function Home() {
   return (
-    <section className="min-h-screen bg-[#fff88e] flex items-start justify-center">
-      {/* Title */}
-      <h1 className="text-[#437dcf] text-8xl bold-text transform scale-105 pt-20 ">NomNomAI</h1>
-      {/* logo */}
-      <div className="mt-5">
-        <Image
-          src="/logo.png"  
-          alt="Logo"
-          width={200}
-          height={200}
-        />
-      </div>
-      <div>
-        {/* add button for speaking here */}
-      </div>  
-      <div>
-        {/* Upload recipe here*/}
+    <section className="h-screen bg-[#fff88e] flex flex-col justify-center items-center">
+      {/* Centered Content */}
+      <div className="flex flex-col items-center gap-6">
+        {/* Title */}
+        <h1 className="text-[#437dcf] text-8xl bold-text transform scale-105">
+          NomNomAI
+        </h1>
+
+        {/* Logo */}
+        <Image src="/logo.png" alt="Logo" width={200} height={200} />
+
+        {/* Button in the Center */}
+        <button className="bg-[#437dcf] text-white px-28 py-7 text-lg rounded-lg shadow-md hover:bg-blue-600 transition">
+          Speak
+        </button>
       </div>
     </section>
   );
