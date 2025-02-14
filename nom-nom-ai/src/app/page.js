e976250c3d28aeb7ce2bc65948d5f29289b044a0
ellaly2 <ellaly2220@gmail.com>
2025-02-14 11:53:55 -0800

    logo added

--- nom-nom-ai/src/app/page.js
+++ nom-nom-ai/src/app/page.js
@@ -1,12 +1,20 @@
 import Image from "next/image";
 import "./App.css";
 
+
 export default function Home() {
   return (
     <section className="min-h-screen bg-[#fff88e] flex items-start justify-center">
+      {/* Title */}
       <h1 className="text-[#437dcf] text-8xl bold-text transform scale-105 pt-20 ">NomNomAI</h1>
-      <div>
-        {/*add logo here*/}
+      {/* logo */}
+      <div className="mt-5">
+        <Image
+          src="/logo.png"  
+          alt="Logo"
+          width={200}
+          height={200}
+        />
       </div>
       <div>
         {/* add button for speaking here */}
